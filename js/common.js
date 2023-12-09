61db1fdbb023f637d6096bb06d1ce7b0da5cbaf7
aibianse <aibianse@outlook.com>
2023-12-09 19:46:54 +0800

    no message

--- js/common.js
+++ js/common.js
@@ -1,22 +1,22 @@
-function pixiv_images() {
-    var img_html = '';
-    for (var i = 1; i <= 50; i++) {
-        img_html += '<img class="nya-btn hot badge" src="./pixiv_images/aibianse_com_' + i + '.webp" referrerpolicy="no-referrer" alt="爱变色"/></br>';
-    }
-    $('#pixiv_images').html(img_html);
-}
+// function pixiv_images() {
+//     var img_html = '';
+//     for (var i = 1; i <= 50; i++) {
+//         img_html += '<img class="nya-btn hot badge" src="./pixiv_images/aibianse_com_' + i + '.webp" referrerpolicy="no-referrer" alt="爱变色"/></br>';
+//     }
+//     $('#pixiv_images').html(img_html);
+// }
 
 function random_video() {
-    $.get("http://127.0.0.1:5000/short_video", function (data) {
-        var url = data.data.rows[0].url.replace('aibianse.pages.dev', 'pixiv.coser.site')
+    $.get("https://api.photograph.show/short_video", function (data) {
+        var url = data.data.rows[0].url.replace('aibianse.pages.dev', 'photograph.show')
         let dom = document.getElementById('random_video')
         dom.src = url
     }, "json");
 }
 
 function random_image() {
-    $.get("http://127.0.0.1:5000/random_image", function (data) {
-        var url = data.data.rows[0].url.replace('aibianse.pages.dev', 'pixiv.coser.site')
+    $.get("https://api.photograph.show/random_image", function (data) {
+        var url = data.data.rows[0].url.replace('aibianse.pages.dev', 'photograph.show')
         let dom = document.getElementById('random_image')
         dom.src = url
     }, "json");
